99259c12f818174d1e16046dc789b6a9b176d8b0
Max.l <a0958057936@gmail.com>
2023-09-28 17:52:41 +0800

    test

--- demo/cname_test/test.py
+++ demo/cname_test/test.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('../..')
-from module.ssl.single_ssl import create_certificate
 from module.dns.dns_pod import create_record
-import asyncio
+from module.ssl.single_ssl import create_certificate
+import concurrent.futures
 
 
 data_template = {
@@ -13,20 +13,11 @@ data_template = {
     "Value": "test1.hqsmaxtest.online"
 }
 
-tasks = []
+tasks = [{**data_template, "SubDomain": f"cnametest{i}"} for i in range(10)]
 
-for i in range(10):
-    data = data_template.copy()
-    data["SubDomain"] = f"cnametest{i}"
-    tasks.append(create_record(data))
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = list(executor.map(create_record, tasks))
 
-asyncio.run(asyncio.gather(*tasks))
-
-certificate_tasks = []
-
-for i in range(10):
-    certificate_tasks.append(create_certificate(f"cnametest{i}.hqsmaxtest.online"))
-asyncio.run(asyncio.gather(*certificate_tasks))
-
-
-
+# 如果 create_certificate 也是同步的，您可以用同样的方法并发执行它
+certificate_tasks = [f"cnametest{i}.hqsmaxtest.online" for i in range(10)]
+certificate_results = list(executor.map(create_certificate, certificate_tasks))
