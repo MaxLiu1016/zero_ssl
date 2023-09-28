752961ee5d2dbc89f493c29076b2b1ea1c3544ba
Max.l <a0958057936@gmail.com>
2023-09-28 18:19:14 +0800

    test

--- demo/cname_test/test.py
+++ demo/cname_test/test.py
@@ -5,22 +5,22 @@ from module.dns.dns_pod import create_record
 import asyncio
 
 
-data_template = {
-    "Domain": "hqsmaxtest.online",
-    "SubDomain": "test",
-    "RecordType": "CNAME",
-    "RecordLine": "默认",
-    "Value": "test1.hqsmaxtest.online"
-}
-
-tasks = []
-
-for i in range(10):
-    data = data_template.copy()
-    data["SubDomain"] = f"cnametest{i}"
-    tasks.append(create_record(data))
-
-asyncio.run(asyncio.gather(*tasks))
+# data_template = {
+#     "Domain": "hqsmaxtest.online",
+#     "SubDomain": "test",
+#     "RecordType": "CNAME",
+#     "RecordLine": "默认",
+#     "Value": "test1.hqsmaxtest.online"
+# }
+#
+# tasks = []
+#
+# for i in range(10):
+#     data = data_template.copy()
+#     data["SubDomain"] = f"cnametest{i}"
+#     tasks.append(create_record(data))
+#
+# asyncio.run(asyncio.gather(*tasks))
 for i in range(10):
     create_certificate(f"cnametest{i}.hqsmaxtest.online")
 
